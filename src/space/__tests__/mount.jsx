d2d14045e8e7039d54b093bb28dd03b2baf73f79
chaishi <974383157@qq.com>
2023-01-06 20:16:43 +0800

    test(space): add unit tests

---
+++ src/space/__tests__/mount.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@test/utils';
+import { Button } from '../../button';
+
+export function getSpaceDefaultMount(Space, props, events) {
+  return render(
+    <Space {...props} {...events}>
+      <Button>Text</Button>
+      <Button>Text</Button>
+      <Button>Text</Button>
+    </Space>
+  );
+}
